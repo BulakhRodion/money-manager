f1595d50226776ca8a7b37deeddf32c2d6c95bcc
Rodion Bulakh <bulakhrodion1@gmail.com>
2022-04-20 18:57:44 +0300

    Router (with routes) added, simple unauthenticated check created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import Welcome from "./views/Welcome/Welcome";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./views/Dashboard/Dashboard";
 
+const currentUser = false; //TODO: Finish user authentication check
+
+const RequireAuth = ({ children }) => {
+  if (currentUser) {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      <Welcome />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route path="login" element={<Welcome />} />
+            <Route index element={<RequireAuth><Dashboard /></RequireAuth>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
