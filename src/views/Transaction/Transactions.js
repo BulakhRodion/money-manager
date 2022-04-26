9624935c5ebb8b28c83f0b0c73b9579a329a65fc
Rodion Bulakh <bulakhrodion1@gmail.com>
2022-04-26 13:50:40 +0300

    Currency data fetched and displayed

--- src/views/Transaction/Transactions.js
+++ src/views/Transaction/Transactions.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {TransactionsContainer} from "./Transactions.styles";
+// import {getCurrencies} from "../../utils/helpers/api";
+import axios from "axios";
+
 
 function Transactions(props) {
+
+    const [currencies, setCurrencies] = useState([]);
+
+    const getCurrencies = () => {
+         axios.get(`https://api.monobank.ua/bank/currency`)
+            .then(res => {
+                setCurrencies(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    // useEffect( () => {
+    //     getCurrencies;
+    // }, []);
+
     return (
         <TransactionsContainer>
             Transaction Page
+            <button onClick={getCurrencies}>Get Currencies</button>
+            {currencies.map((currency, index) => {
+                return (
+                    <div key={index}>
+                        {currency.currencyCodeA} {currency.currencyCodeB}
+                    </div>
+                )
+            })}
         </TransactionsContainer>
     );
 }
